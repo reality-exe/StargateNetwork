3ef6aaf95569a161cfbdc40239dcee59dcdb0596
reality-exe <detectivebott@gmail.com>
2023-11-08 22:16:17 +0000

    Changed websocket messages, added 403 if duplicated session

--- index.js
+++ index.js
@@ -26,34 +26,41 @@ wss.on("connection", async (ws) => {
     console.log(payload);
     let new_data = payload.new;
 
-    switch (new_data.gate_status) {
-      case "IDLE":
-        if (sessionData.incoming) {
-          ws.send("Close wormhole");
-          sessionData.incoming = false;
+    if (new_data.gate_address == sessionData.address) {
+      switch (new_data.gate_status) {
+        case "IDLE":
+          if (sessionData.incoming) {
+            sessionData.incoming = false;
+            ws.send("Impulse:CloseWormhole");
+            break;
+          }
           break;
-        }
-        break;
-      case "OUTGOING":
-        break;
-      case "INCOMING":
-        ws.send("Incoming wormhole");
-        sessionData.incoming = true;
-        break;
+        case "OUTGOING":
+          break;
+        case "INCOMING":
+          sessionData.incoming = true;
+          ws.send("Impulse:OpenIncoming");
+          break;
 
-      default:
-        break;
+        default:
+          break;
+      }
     }
   }
 
   ws.on("message", async (msg) => {
     let json = JSON.parse(msg.toString());
+    console.log(json)
     switch (json.type) {
       case "validateAddress":
         // Return 400 if address' length is too short
         if (json.gate_address.length < 6) {
-          ws.send('{"code":400,"message":"Address too short"}');
+          ws.send("CSValidCheck:400");
           break;
+        }
+
+        if (json.gate_address.length > 6) {
+          let gate_code = json.gate_address.slice(0, 6);
         }
 
         var { data: data, error } = await supabase
@@ -64,34 +71,41 @@ wss.on("connection", async (ws) => {
 
         if (data == null) {
           // Return 404 if there is no gate with that address
-          ws.send('{"code":404, "message":"Gate not found"}');
+          ws.send("CSValidCheck:404");
           break;
         } // Return 302 if the outgoing gate has a different group code from the dialing gate
-        else if (data.gate_code != json.gate_code) {
-          ws.send('{"code":302,"message":"Wrong gate code"}');
+        else if (data.gate_code != "") {
+          ws.send("CSValidCheck:302");
           break;
         } // Return 200 if gate exists, and is not already open
         else if (data.gate_status == "IDLE") {
-          ws.send('{"code":200,"message":"Address valid"}');
+          ws.send("CSValidCheck:200");
           break;
         } // Return 403 if gate has an active wormhole
         else if (data.gate_status == "OPEN") {
-          ws.send('{"code":403,"message":"Gate busy"}');
+          ws.send("CSValidCheck:403");
           break;
         }
 
         break;
       case "requestAddress":
         console.log("Request address");
+        console.log(json)
         if (json.gate_address.length < 6) {
           ws.send('{ code: 400, message: "Address too short" }');
           break;
         }
-        var { data: data } = await supabase
+        var { data: data, error } = await supabase
           .from("gates")
           .select("*")
           .eq("gate_address", json.gate_address)
           .single();
+        console.log(error ?? "No error");
+
+        if (data.session_id == json.session_id) {
+          ws.send("403");
+          break;
+        }
 
         if (data == null) {
           sessionData.address = json.gate_address;
@@ -103,8 +117,8 @@ wss.on("connection", async (ws) => {
               is_headless: json.is_headless,
               session_id: json.session_id,
               gate_address: json.gate_address,
-              gate_code: json.gate_code,
               gate_status: "IDLE",
+              gate_code: json.gate_code,
               current_users: json.current_users,
               max_users: json.max_users,
               public_gate: json.public,
@@ -124,12 +138,12 @@ wss.on("connection", async (ws) => {
 
           ws.send('{ code: 200, message: "Address accepted" }');
         } else {
-          ws.send('{ code: 403, message: "Address already taken" }');
+          ws.send("403");
           break;
         }
         break;
       case "dialRequest":
-        if (json.dialed_address == sessionData.address) {
+        if (json.gate_address == sessionData.address) {
           ws.send('{"code": 403, "message":"Gate is busy"}');
           break;
         }
@@ -137,32 +151,58 @@ wss.on("connection", async (ws) => {
         var { data: data, error } = await supabase
           .from("gates")
           .select("*")
-          .eq("gate_address", json.dialed_address)
-          .eq("gate_code", json.dialed_code)
+          .eq("gate_address", json.gate_address)
+          // .eq("gate_code", json.dialed_code)
           .single();
         if (data == null) {
-          ws.send('{"code":404, "message":"Gate not found"}');
+          ws.send("CSDialCheck:404");
           break;
         }
 
         if (data.gate_status != "IDLE") {
-          ws.send('{"code": 403, "message":"Gate is busy"}');
+          ws.send("CSDialCheck:403");
           break;
         } else {
           var { data: d, error } = await supabase
             .from("gates")
             .update({ gate_status: "INCOMING" })
-            .eq("gate_address", json.dialed_address)
-            .eq("gate_code", json.dialed_code)
+            .eq("gate_address", json.gate_address)
+            // .eq("gate_code", json.dialed_code)
             .select();
-          console.log(json.dialed_address);
+          console.log(json.gate_address);
 
-          ws.send('{"code":200, "message":"Wormhole established"}');
+          ws.send("CSDialCheck:200");
+          ws.send(`CSDialedSessionURL:${data.session_id}`);
         }
         break;
       case "keepAlive":
         break;
       case "closeWormhole":
+        var { data: _this, error } = await supabase
+          .from("gates")
+          .update({ gate_status: "IDLE" })
+          .eq("gate_address", sessionData.address)
+          .eq("gate_code", sessionData.code)
+          .select();
+
+        var { data: _other, error } = await supabase
+          .from("gates")
+          .update({ gate_status: "IDLE" })
+          .eq("gate_address", sessionData.dialed_session.address)
+          .eq("gate_code", sessionData.dialed_session.code)
+          .select();
+        break;
+      case "updateData":
+        var { data: data, error } = await supabase
+          .from("gates")
+          .update({
+            gate_status: json.gate_status,
+            current_users: json.current_users,
+            max_users: json.max_users,
+          })
+          .eq("gate_address", sessionData.address)
+          .select();
+        console.log(error ?? "e");
         break;
       case "test":
         console.log("Recieved test");
@@ -180,7 +220,10 @@ wss.on("connection", async (ws) => {
     );
     if (sessionData.address != "") {
       db_channel.unsubscribe();
-      await supabase.from("gates").delete().eq("host_id", sessionData.host_id);
+      await supabase
+        .from("gates")
+        .delete()
+        .eq("gate_address", sessionData.address);
     }
   });
 });
