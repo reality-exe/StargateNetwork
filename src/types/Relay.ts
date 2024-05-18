529d4873fcba4a59e86063aae8d29772497324c0
Reality <reality@rxserver.net>
2024-05-18 17:01:22 -0400

    Add types

---
+++ src/types/Relay.ts
@@ -0,0 +1,5 @@
+export type Relay = {
+    relay: string;
+    from: string;
+    to: string;
+}
---
+++ src/types/message-types/Messages.ts
@@ -0,0 +1,41 @@
+export type CloseWormhole = {
+  type: string;
+};
+
+export type DialRequest = {
+  type: string;
+  gate_address: string;
+};
+export type ValidateAddress = {
+  type: string;
+  gate_address: string;
+};
+
+export type RequestAddress = {
+  type: string;
+  gate_address: string;
+  gate_code: string;
+  host_id: string;
+  is_headless: boolean;
+  session_id: string;
+  current_users: number;
+  max_users: number;
+  public: boolean;
+  gate_name: string;
+};
+
+export type UpdateData = {
+  type: string;
+  currentUsers: number;
+  maxUsers: number;
+  gate_status: string;
+  irisState: boolean;
+};
+
+export enum Type {
+  RequestAddress = "requestAddress",
+  ValidateAddress = "validateAddress",
+  DialRequest = "dialRequest",
+  UpdateData = "updateData",
+  CloseWormhole = "closeWormhole",
+}
