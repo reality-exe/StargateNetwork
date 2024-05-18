cec1ad6753752d0218cfd5af248e6aadf2cb1945
Reality <reality@rxserver.net>
2024-05-18 17:01:18 -0400

    Add pocketbase service

---
+++ src/services/pocketbase.service.ts
@@ -0,0 +1,39 @@
+import "dotenv/config";
+import PocketBase from "pocketbase/cjs";
+import { Stargate } from "../types/Stargate";
+const { WS_PORT, PB_EMAIL, PB_ENDPOINT, PB_PASSWORD } = process.env;
+
+const pb = new PocketBase("https://aor-db.rxserver.net");
+
+async function auth() {
+  await pb.admins
+    .authWithPassword(PB_EMAIL as string, PB_PASSWORD as string)
+    .then((res) => {
+      if (res) {
+        pb.autoCancellation(false);
+        console.log(
+          `${new Date()} | Logged into PocketBase instance. Email: ${
+            res.admin.email
+          }`
+        );
+      }
+    });
+}
+auth();
+export default pb;
+
+export async function findGate(gate_address: string) {
+  try {
+    let response = await pb
+      .collection("stargates")
+      .getFirstListItem<Stargate>(`gate_address="${gate_address}"`);
+    return response;
+  } catch (error) {
+    return null;
+  }
+}
+
+export async function createGate(data: Stargate) {
+  let response = await pb.collection("stargate").create<Stargate>(data);
+  return response;
+}
